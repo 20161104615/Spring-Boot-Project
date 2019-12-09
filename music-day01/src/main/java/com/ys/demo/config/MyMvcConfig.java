7b1f9a3b75f42027f5e574b708ad8d3fef48da94
20161104615 <ys1019172727@vip.qq.com>
2019-12-09 15:15:48 +0800

    day04
    Ajax登录、过滤器

--- music-day01/src/main/java/com/ys/demo/config/MyMvcConfig.java
+++ music-day01/src/main/java/com/ys/demo/config/MyMvcConfig.java
@@ -1,9 +1,11 @@
 package com.ys.demo.config;
 
+import com.ys.demo.component.LoginHandlerInterceptor;
 import com.ys.demo.component.MyLocaleResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,22 +13,29 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MyMvcConfig implements WebMvcConfigurer {
 
     @Bean//将组件注册在容器中
-    public WebMvcConfigurer webMvcConfigurer(){
+    public WebMvcConfigurer webMvcConfigurer() {
         WebMvcConfigurer adapter = new WebMvcConfigurer() {
-          @Override
-          public void addViewControllers(ViewControllerRegistry registry){
-              registry.addViewController("/index.html").setViewName("index");
-              registry.addViewController("/index").setViewName("index");
-              registry.addViewController("/signin.html").setViewName("signin");
-              registry.addViewController("/signup.html").setViewName("signup");
-              registry.addViewController("/modal.lockme.html").setViewName("signin");
-          }
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/index.html").setViewName("index");
+                registry.addViewController("/index").setViewName("index");
+                registry.addViewController("/signin.html").setViewName("signin");
+                registry.addViewController("/signup.html").setViewName("signup");
+                registry.addViewController("/modal.lockme.html").setViewName("signin");
+            }
+
+            //拦截器注册
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/signup.html").
+                        excludePathPatterns("/","/signin.html","/user/login","/modal.lockme.html");
+            }
         };
         return adapter;
     }
 
     @Bean
-    public LocaleResolver localeResolver(){
+    public LocaleResolver localeResolver() {
         return new MyLocaleResolver();
     }
 }
--- music-day01/src/main/java/com/ys/demo/controller/UserController.java
+++ music-day01/src/main/java/com/ys/demo/controller/UserController.java
@@ -1,32 +1,48 @@
 package com.ys.demo.controller;
 
-
-import org.springframework.stereotype.Controller;
+import net.sf.json.JSONObject;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.thymeleaf.util.StringUtils;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.Map;
 
 //@ResponseBody//这个类得所有的方法返回的数据直接写给浏览器（如果是对象转为json数据）
 //@Controller
 //@RestController = @ResponseBody + @Controller
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
 
     @PostMapping(value = "/login")
-    public String userLogin(@RequestParam("user_email") String user_email,
-                             @RequestParam("user_password") String user_password,
-                             Map<String,Object> map) {
-        if(!StringUtils.isEmpty(user_email) && "123".equals(user_password)){
+    public void userLogin(@RequestParam("user_email") String user_email,
+                          @RequestParam("user_password") String user_password,
+                          Map<String, Object> map, HttpSession session,
+                          HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.setCharacterEncoding("utf-8");
+        response.setContentType("text/html;charset=utf-8");
+        JSONObject jsonObject;
+        System.out.println("进入函数userLogin");
+        System.out.println("用户邮箱："+user_email+"用户密码："+user_password);
+        if (!StringUtils.isEmpty(user_email) && "123".equals(user_password)) {
             //登录成功,防止二次提交表单
-            return "redirect:/index.html";
-        } else{
+            System.out.println("密码正确");
+            session.setAttribute("loginUser", user_email);
+            jsonObject = JSONObject.fromObject(map);
+            response.getWriter().print(jsonObject);
+        } else {
             //登录失败
-            map.put("emsg","用户名或密码错误");
-            return "signin.html";
+            System.out.println("密码错误");
+            map.put("stat","0");
+            jsonObject = JSONObject.fromObject(map);
+            response.getWriter().print(jsonObject);
         }
     }
 }
